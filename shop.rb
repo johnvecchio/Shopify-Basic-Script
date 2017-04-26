4a6cbf6ff0d35ba1bba7c84f66bd533643f92e1a
johnvecchio <johnvecchio24@gmail.com>
2017-04-26 13:33:44 -0700

    Update shop.rb

--- shop.rb
+++ shop.rb
@@ -1,15 +1,15 @@
 customer = Input.cart.customer
-discount = 1
+discount = 1 # Discount start variable 
 
-# Non or diff discount array
+# You can use this array to make sure these items are not discounted
 array = [8294234817, 8294234753, 8294234433, 8313434113, 8294233281, 8294233345, 8294269633, 8294268993]
 
-# put any sale ID in here for
+# This function checks for specialty items that you might not want your Customers applied coupons to
 def sale()
     itemCount = 0
-    if Input.cart.discount_code == nil
-      puts ""
-    else
+    if Input.cart.discount_code == nil # IF the cart has a discount code
+      puts "" # Shopify would not allow the proper logic here so this is a work around IF statement
+    else # IF NOT remove it
       Input.cart.line_items.each do |line_item|
         product = line_item.variant.product
         if (product.id == 9556534093)
@@ -20,7 +20,9 @@ def sale()
 end
 
 
-
+# The logic below works like this >
+#       IF there are using a customer account (non guest checkout) > check if they are tagged for discounts > then check further discounts
+#       IF NOT (theyre using a guest checkout) > still check to see if they are trying to use a promo code on a special item
 if customer
     customer.tags.each do |tag|
       if tag.include?("ProDeal")
@@ -31,7 +33,10 @@ if customer
         discount = 0.28
       end
   end
- # if there is a discount code, dont give them a discount beyond that
+    
+ # The commented out can be used if desired. This IF statement checks to see if there is already a discount applied.
+ # LOGIC: If there is a discount code > do NOT give them a discount beyond that
+
  # if Input.cart.discount_code == nil
     # Cycle through all products in the cart to check
     Input.cart.line_items.each do |line_item|
@@ -41,9 +46,9 @@ if customer
       end
     end
  # end
-        sale()
+    sale() # checking for further discount on special / sale items
 else
-  sale()
+  sale() # checking for further discount on special / sale items
 end # End
 
 # Output to the Cart the new changes
